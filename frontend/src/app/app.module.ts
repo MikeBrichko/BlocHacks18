776c5a835ba7ef0ccfa99a73d90026fd26304b9f
Mike Brichko <w.mike.brichko@gmailcom>
2018-11-17 15:55:28 -0500

    Adding components for donor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RequestListComponent } from './request-list/request-list.component';
+import { RequestCardComponent } from './request-list/request-card/request-card.component';
+import { RequestDetailsComponent } from './request-details/request-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RequestListComponent,
+    RequestCardComponent,
+    RequestDetailsComponent
   ],
   imports: [
     BrowserModule
