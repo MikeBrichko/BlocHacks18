ea21c57d9005397a64d990c16c4120e49fa27f58
= <yth6339@hotmail.com>
2018-11-17 12:06:05 -0500

    init backend flask

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port, debug=True)
