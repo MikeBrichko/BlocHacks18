0cf77856658bfba5622d04e4502ae40448aa0982
= <yth6339@hotmail.com>
2018-11-17 21:35:17 -0500

    backend port changes

--- backend/main.py
+++ backend/main.py
@@ -111,8 +111,8 @@ def after_request(response):
 	
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
-    port = int(os.environ.get('PORT', 8090))
-    app.run(host='127.0.0.1', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='127.0.0.1', port=port)
 
 
 
